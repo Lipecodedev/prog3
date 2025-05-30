75f62794b975951837b53f77dbfea87635531b9f
Lipecodedev <felipesantos@soulasalle.com.br>
2025-05-29 21:07:43 -0300

    Create veiculo.java

---
+++ semana11/exercicio2/dominio/veiculo.java
@@ -0,0 +1,48 @@
+
+package dominio;
+
+public class Veiculo {
+    protected String marca;
+    protected String modelo;
+    protected int ano;
+
+    // Construtor
+    public Veiculo(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    // Getters
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    // Setters
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    // Método para exibir os dados
+    public void exibirDados() {
+        System.out.println("Marca: " + getMarca());
+        System.out.println("Modelo: " + getModelo());
+        System.out.println("Ano: " + getAno());
+    }
+}
