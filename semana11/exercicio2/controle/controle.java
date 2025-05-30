aedbba51232682bfbeceb6bafc10e1d3670e14e0
Lipecodedev <felipesantos@soulasalle.com.br>
2025-05-29 21:07:06 -0300

    Create controle.java

---
+++ semana11/exercicio2/controle/controle.java
@@ -0,0 +1,16 @@
+
+    package controle;
+
+    import dominio.*;
+    
+    public class controle {
+        public static void main(String[] args) {
+            Carro carro = new Carro("toyota","corolla",2004,4);
+            Moto moto  = new Moto("yamaha","fazer",2004,600;
+           
+    
+            System.out.println("marca do carro: " +carro.getMarca()+"\nmodelo do carro:"+carro.getModelo()+ "\n ano do carro:" + carro.getAno()+"quantidades de portas:"+carro.getQuantidadePortas();
+            System.out.println("marca da moto: " +moto.getMarca()+"\nmodelo da moto:"+moto.getModelo()+ "\n ano da moto:" + moto.getAno()+"cilindradas:"+moto.getCilindradas();
+
+        }
+    }
